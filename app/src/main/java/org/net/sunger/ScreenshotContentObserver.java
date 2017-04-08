d0417fd1c2a93b308e4a996de2a4a5beedf5563c
sunguorong <sgr123!@#>
2017-04-08 13:53:10 +0800

    update

--- app/src/main/java/org/net/sunger/ScreenshotContentObserver.java
+++ app/src/main/java/org/net/sunger/ScreenshotContentObserver.java
@@ -13,12 +13,17 @@ import android.provider.MediaStore;
  * Created by sunger on 2017/4/7.
  */
 public class ScreenshotContentObserver {
+    //截图文件名关键字
     private static final String[] KEYWORDS = {"screenshot", "screen_shot", "screen-shot", "screen shot", "screencapture", "screen_capture", "screen-capture", "screen capture", "screencap", "screen_cap", "screen-cap", "screen cap", "截屏"};
+    //内容观察者中读取的信息
     private static final String[] MEDIA_PHOTO = {MediaStore.Images.ImageColumns.DATA, MediaStore.Images.ImageColumns.DATE_TAKEN, MediaStore.MediaColumns.WIDTH, MediaStore.MediaColumns.HEIGHT};
     private Activity mContext;
     private Handler mHandler;
+    //内置存储器观察者
     private ContentObserver mInternalObserver;
+    //外置存储器内容观察者
     private ContentObserver mExternalObserver;
+    //回调
     private ICallBack iCallBack;
 
     public ScreenshotContentObserver(Activity context) {
@@ -30,13 +35,16 @@ public class ScreenshotContentObserver {
         HandlerThread mHandlerThread = new HandlerThread("Screenshot_Observer");
         mHandlerThread.start();
         mHandler = new Handler(mHandlerThread.getLooper());
+        //初始化
         mInternalObserver = new MediaContentObserver(MediaStore.Images.Media.INTERNAL_CONTENT_URI, mHandler);
         mExternalObserver = new MediaContentObserver(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, mHandler);
+        //注册
         mContext.getContentResolver().registerContentObserver(MediaStore.Images.Media.INTERNAL_CONTENT_URI, false, mInternalObserver);
         mContext.getContentResolver().registerContentObserver(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, false, mExternalObserver);
     }
 
     public void stopObserve() {
+        //取消注册
         if (mInternalObserver != null) {
             mContext.getContentResolver().unregisterContentObserver(mInternalObserver);
             mContext.getContentResolver().unregisterContentObserver(mExternalObserver);
@@ -74,12 +82,14 @@ public class ScreenshotContentObserver {
                     null,
                     MediaStore.Images.ImageColumns.DATE_ADDED + " desc limit 1"
             );
+            //判断是否为空
             if (cursor == null) {
                 return;
             }
             if (!cursor.moveToFirst()) {
                 return;
             }
+            //获取图片的路径，创建时间，分辨率
             int dataIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
             int dateTakenIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATE_TAKEN);
             int dataWidth = cursor.getColumnIndex(MediaStore.MediaColumns.WIDTH);
@@ -90,6 +100,7 @@ public class ScreenshotContentObserver {
             int width = cursor.getInt(dataWidth);
             int height = cursor.getInt(dataHeight);
             Point point = new Point(width, height);
+            //根据条件判断是不是截图
             if (matchAddTime(dateTaken) && matchPath(data) && matchSize(point)) {
                 if (iCallBack != null) {
                     iCallBack.onScreenShot(data);
@@ -105,6 +116,7 @@ public class ScreenshotContentObserver {
     }
 
 
+    //自定义内容观察者
     private class MediaContentObserver extends ContentObserver {
         private Uri mContentUri;
 
